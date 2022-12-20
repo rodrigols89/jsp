51cb77b0a6e819041a8005fe9442191aa520a664
drigols <drigols.creative@gmail.com>
2022-12-20 13:05:55 -0300

    Add pre-commit settings Add pre-commit Add black add flake8 add reorder-python-import add pyupgrade Add .pre-commit-config.yaml file

--- src/Training.py
+++ src/Training.py
@@ -1,41 +1,48 @@
+from __future__ import annotations
+
 from datetime import datetime
 
-from sklearn.model_selection import train_test_split
+import pandas as pd
 from catboost import CatBoostRegressor
+from sklearn.model_selection import train_test_split
 
 
 class Training:
-    
-    def split_data(self, x, y):
+    def split_data(self, X, y):
 
         start_time = datetime.now()
 
-        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.3, random_state=42)
+        X_train, X_valid, y_train, y_valid = train_test_split(
+            X, y, test_size=0.3, random_state=42
+        )
         print("Data splitted!")
 
         end_time = datetime.now()
-        print('Method runtime: {}'.format(end_time - start_time))
+        print(f"Method runtime: {end_time - start_time}")
 
         return X_train, X_valid, y_train, y_valid
 
-
-
-    def catBoostRegressor(self, X_train, y_train, X_valid_or_test, cat_features=[0], predict=False):
+    def catBoostRegressor(
+        self,
+        X_train,
+        y_train,
+        X_valid_or_test,
+        cat_features=[0],
+        predict=False,
+    ):
 
         start_time = datetime.now()
-        
-        train_data = X_train # X data.
-        train_labels = y_train # y data.
-        
+
+        train_data = X_train  # X data.
+        train_labels = y_train  # y data.
+
         model = CatBoostRegressor()
-        model.fit(
-            train_data,
-            train_labels,
-            cat_features
-        )
-        
+        model.fit(train_data, train_labels, cat_features)
+
         if predict is True:
-            salaries_predicted = pd.DataFrame(model.predict(X_valid_or_test), columns=["SalaryNormalized"])
+            salaries_predicted = pd.DataFrame(
+                model.predict(X_valid_or_test), columns=["SalaryNormalized"]
+            )
             end_time = datetime.now()
-            print('Method runtime: {}'.format(end_time - start_time))
+            print(f"Method runtime: {end_time - start_time}")
             return salaries_predicted
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,60 +1,61 @@
-import pandas as pd
-import platform
-import py7zr
+from __future__ import annotations
 
+import platform
 from datetime import datetime
 
-class Preprocessing:
+import pandas as pd
+import py7zr
 
+
+class Preprocessing:
     def extract_7z_data(self, path):
-        
+
         start_time = datetime.now()
-        
+
         # For Windows users.
-        if platform.system() == 'Windows':
+        if platform.system() == "Windows":
             try:
 
-                with py7zr.SevenZipFile(path, mode='r') as archive:
-                    archive.extractall(path="C:\Windows\Temp")
+                with py7zr.SevenZipFile(path, mode="r") as archive:
+                    archive.extractall(path=r"C:\Windows\Temp")
             except FileNotFoundError:
                 print("File or path not found!")
             else:
                 print("File extracted!")
         # For Linux users.
-        elif platform.system() == 'Linux':
+        elif platform.system() == "Linux":
             try:
-                with py7zr.SevenZipFile(path, mode='r') as archive:
+                with py7zr.SevenZipFile(path, mode="r") as archive:
                     archive.extractall(path="/tmp")
             except FileNotFoundError:
                 print("File or path not found!")
             else:
                 print("File extracted!")
         else:
-            print("This method only works with Windows and Linux Operating Systems.")
-        
+            print("This method only works with Windows and Linux O.S.")
+
         # Time used to extract the file.
         end_time = datetime.now()
-        print('Method runtime: {}'.format(end_time - start_time))
+        print(f"Method runtime: {end_time - start_time}")
 
-        
     def get_training_data(self):
 
         start_time = datetime.now()
 
         # For Windows users.
-        if platform.system() == 'Windows':
+        if platform.system() == "Windows":
             try:
-                df_training = pd.read_csv("C:\Windows\Temp\Train_rev1.csv")
+                df_training = pd.read_csv(r"C:\Windows\Temp\Train_rev1.csv")
             except FileNotFoundError:
                 print("File or path not found!")
             else:
                 print("Training data is ready!")
                 # Time used to get the file.
                 end_time = datetime.now()
-                print('Method runtime: {}'.format(end_time - start_time))
+                print(f"Method runtime: {end_time - start_time}")
                 return df_training
         # For Linux users.
-        elif platform.system() == 'Linux':
+        elif platform.system() == "Linux":
             try:
                 df_training = pd.read_csv("/tmp/Train_rev1.csv")
             except FileNotFoundError:
@@ -63,28 +64,27 @@ class Preprocessing:
                 print("Training data is ready!")
                 # Time used to get the file.
                 end_time = datetime.now()
-                print('Method runtime: {}'.format(end_time - start_time))
+                print(f"Method runtime: {end_time - start_time}")
                 return df_training
 
-
     def get_testing_data(self):
-        
+
         start_time = datetime.now()
-        
+
         # For Windows users.
-        if platform.system() == 'Windows':
+        if platform.system() == "Windows":
             try:
-                df_testing = pd.read_csv("C:\Windows\Temp\Test_rev1.csv")
+                df_testing = pd.read_csv(r"C:\Windows\Temp\Test_rev1.csv")
             except FileNotFoundError:
                 print("File or path not found!")
             else:
                 print("Testing data ready!")
                 # Time used to get the file.
                 end_time = datetime.now()
-                print('Method runtime: {}'.format(end_time - start_time))
+                print(f"Method runtime: {end_time - start_time}")
                 return df_testing
         # For Linux users.
-        elif platform.system() == 'Linux':
+        elif platform.system() == "Linux":
             try:
                 df_testing = pd.read_csv("/tmp/Test_rev1.7z")
             except FileNotFoundError:
@@ -93,5 +93,5 @@ class Preprocessing:
                 print("Testing data ready!")
                 # Time used to get the file.
                 end_time = datetime.now()
-                print('Method runtime: {}'.format(end_time - start_time))
+                print(f"Method runtime: {end_time - start_time}")
                 return df_testing
