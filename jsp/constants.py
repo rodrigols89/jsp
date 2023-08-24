8327fca7844b4ed7938f3cdd75423b66c7bc591d
drigols <drigols.creative@gmail.com>
2023-08-24 15:56:03 -0300

    refactor(constants): Remove link from constants.py

--- jsp/constants.py
+++ jsp/constants.py
@@ -1,4 +1,1 @@
 from __future__ import annotations
-
-train_url = "https://www.kaggle.com/competitions/job-salary-prediction/data?select=Train_rev1.7z"  # noqa
-test_url = "https://www.kaggle.com/competitions/job-salary-prediction/data?select=Test_rev1.7z"  # noqa
