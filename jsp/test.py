ca7819e1be70bea95d07cb1a820ea4b480732d6b
drigols <drigols.creative@gmail.com>
2023-09-07 17:48:40 -0300

    test(ETL) Add manual test to load dataframe functions.

--- jsp/test.py
+++ jsp/test.py
@@ -6,8 +6,11 @@
 from __future__ import annotations
 
 # from jsp.database.connections import get_engine_connection
-from jsp.etl.extract import extract_data_from_csv
-from jsp.etl.load import load_data_to_postgresql
+# from jsp.etl.extract import extract_data_from_csv
+# from jsp.etl.load import (
+#    load_test_df_to_postgresql,
+#    load_train_df_to_postgresql,
+# )
 
 if __name__ == "__main__":
     pass
@@ -19,11 +22,11 @@ if __name__ == "__main__":
     # print("Returned value type:", type(engine))
 
     """Testing extract_data_from_csv() function."""
-    train_df = extract_data_from_csv("jsp/datalake/landing/Train_rev1.csv")
-    test_df = extract_data_from_csv("jsp/datalake/landing/Test_rev1.csv")
+    # train_df = extract_data_from_csv("jsp/datalake/landing/Train_rev1.csv")
+    # test_df = extract_data_from_csv("jsp/datalake/landing/Test_rev1.csv")
     # print(train_df)
     # print(test_df)
 
-    """Testing load_data_to_postgresql() function."""
-    load_data_to_postgresql("train-table", train_df)
-    load_data_to_postgresql("test-table", test_df)
+    """Testing load_train/test_df_to_postgresql() functions."""
+    # load_train_df_to_postgresql()
+    # load_test_df_to_postgresql()
