9af903f20a31fa43ce32d313611f3a414d7be867
drigols <drigols.creative@gmail.com>
2023-09-08 19:32:48 -0300

    refactor(ETL) Add exceptions to extract_data_from_csv() function

--- jsp/etl/extract.py
+++ jsp/etl/extract.py
@@ -9,15 +9,34 @@ from __future__ import annotations
 import pandas as pd
 
 
-def extract_data_from_csv(csv_file: str) -> pd.DataFrame:
+def extract_data_from_csv(csv_file_path: str) -> pd.DataFrame:
     """
     Extracts data from a CSV file and returns it as a pandas DataFrame.
 
     Args:
-        csv_file (str): The path to the CSV file.
+        csv_file_path (str): The path to the CSV file.
+
+    Raises:
+        FileNotFoundError:
+            If the .CSV file or directory is not found.
+        PermissionError:
+            If there is a permission error accessing the .CSV file.
+        pd.errors.EmptyDataError:
+            If the .CSV file is empty.
+        pd.errors.ParserError:
+            If there is an error parsing the .CSV file.
 
     Returns:
         pd.DataFrame: The data extracted from the .CSV file.
     """
-    df = pd.read_csv(csv_file)
-    return df
+    try:
+        data_frame = pd.read_csv(csv_file_path)
+        return data_frame
+    except FileNotFoundError as error:
+        raise error
+    except PermissionError as error:
+        raise error
+    except pd.errors.EmptyDataError as error:
+        raise error
+    except pd.errors.ParserError as error:
+        raise error
