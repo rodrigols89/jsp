a330fc6dfde65f1602a5581e8e2ffe9edd7c3582
drigols <drigols.creative@gmail.com>
2023-09-07 22:41:02 -0300

    feat(CLI): Add load commands to load the Dataframes into database.

---
+++ jsp/cli/etl.py
@@ -0,0 +1,52 @@
+# Authors: Rodrigo Leite <drigols.creative@gmail.com>
+#
+# License: MIT
+# ============================================================================
+"""Module to apply ETL commands."""
+from __future__ import annotations
+
+import typer
+
+from jsp.etl.load import (
+    load_test_df_to_postgresql,
+    load_train_df_to_postgresql,
+)
+
+app = typer.Typer(help="Command to apply ETL processes.")
+
+
+@app.command("load-train")
+def load_train() -> None:
+    """
+    Loads train DataFrames into PostgreSQL.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    load_train_df_to_postgresql()
+
+
+@app.command("load-test")
+def load_test() -> None:
+    """
+    Loads test data into PostgreSQL.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    load_test_df_to_postgresql()
+
+
+@app.command("load-all")
+def load_all() -> None:
+    """
+    Loads all train and test data into PostgreSQL.
+    """
+    load_train()
+    load_test()
