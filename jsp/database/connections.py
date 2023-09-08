d97caba6113ec4993493e7455d9d2eccd1be3ae3
drigols <drigols.creative@gmail.com>
2023-09-08 12:48:17 -0300

    refactor(database): Add exceptions handling to get_engine_connection() function

--- jsp/database/connections.py
+++ jsp/database/connections.py
@@ -5,7 +5,9 @@
 """Module to work with Database connections."""
 from __future__ import annotations
 
-from sqlalchemy import Engine, create_engine
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 
 
 def get_engine_connection(
@@ -20,12 +22,27 @@ def get_engine_connection(
     Create a SQLAlchemy Engine connection.
 
     Args:
-        dialect (str, optional): The database dialect. Defaults to "postgresql".
-        username (str, optional): The database username. Defaults to "postgres".
-        password (str, optional): The database password. Defaults to "postgres".
-        database (str, optional): The name of the database. Defaults to "jsp-db".
-        host (str, optional): The database host. Defaults to "localhost".
-        port (int, optional): The database port. Defaults to 5432.
+        dialect (str, optional):
+            The database dialect. Defaults to "postgresql".
+        username (str, optional):
+            The database username. Defaults to "postgres".
+        password (str, optional):
+            The database password. Defaults to "postgres".
+        database (str, optional):
+            Name of the database. Defaults to "jsp-db".
+        host (str, optional):
+            The database host. Defaults to "localhost".
+        port (int, optional):
+            The database port. Defaults to 5432.
+
+    Raises:
+        ValueError:
+            Raise when invalid values are passed. E.g. port="ABC".
+        sqlalchemy.exc.NoSuchModuleError:
+            Raise when invalid dialect are passed. E.g. dialect=102030.
+            Raise when missing driver modules.
+        sqlalchemy.exc.ArgumentError:
+            Raise when missing value for dialect argument. E.g. dialect=""
 
     Returns:
         Engine: A SQLAlchemy Engine object.
@@ -34,11 +51,16 @@ def get_engine_connection(
         >>> engine = get_engine_connection()
         >>> print(engine)
         Engine(postgresql://postgres:***@localhost:5432/jsp-db)
-
-    # noqa
     """
-    engine = create_engine(
-        f"{dialect}://{username}:{password}@{host}:{port}/{database}",
-        echo=False,
-    )
-    return engine
+    try:
+        connection_url = (
+            f"{dialect}://{username}:{password}@{host}:{port}/{database}"
+        )
+        engine = create_engine(connection_url, echo=False)
+        return engine
+    except ValueError as error:
+        raise error
+    except sqlalchemy.exc.NoSuchModuleError as error:
+        raise error
+    except sqlalchemy.exc.ArgumentError as error:
+        raise error
